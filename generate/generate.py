5300606959ded096689778c5f8df90c156b0cb11
Josef Utbult <josef@utbult.design>
2025-04-15 20:43:55 +0200

    Try to get github workflow to workflow

--- generate/generate.py
+++ generate/generate.py
@@ -6,6 +6,7 @@ def setup():
     OUTPUT_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
 if __name__ == '__main__':
+    print(f"Generating static site into {OUTPUT_DIRECTORY}")
     setup()
 
     with open(OUTPUT_DIRECTORY / Path('index.html'), 'w') as index:
